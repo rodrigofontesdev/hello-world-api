166bb1dd24189c4b398e8a596ef5cf101dce2b50
Rodrigo Fontes <fnts.rodrigo.dev@gmail.com>
2024-08-16 21:52:45 -0300

    rodando múltiplos containers

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
+      host: 'mysql-rocket',
       port: 3306,
       username: 'admin',
       password: 'root',
